3bd178424f24f7a296e5042772e1cd80864da20b
Yugang Zhou <hippoom@gmail.com>
2026-07-03 17:02:45 +0800

    Revert "Add Unicode fonts module and Chinese unit name translation from thezephyrsong"
    This reverts commit ac5e9cc3f71459599e618fe97a6c7b6cf38b3b15.

--- core/core.lua
+++ core/core.lua
@@ -131,8 +131,6 @@ function DFRL:CheckAddon(name)
         self.addon3 = true
     elseif name == "Immersion" then
         self.addon4 = true
-    elseif name == "UnicodeFont" then
-        self.addon5 = true
     end
 
     if IsAddOnLoaded("ShaguTweaks") then
@@ -146,9 +144,6 @@ function DFRL:CheckAddon(name)
     end
     if IsAddOnLoaded("Immersion") then
         self.addon4 = true
-    end
-    if IsAddOnLoaded("UnicodeFont") then
-        self.addon5 = true
     end
 
     self:UpdateShaguReady()
